406217d4a85cea0f9b252e947b10067ebe274f9b
Dennis Brink <dennis.schack@gmail.com>
2019-02-20 12:51:05 +0100

    Adjustments to teateret.

--- theroom/runtime/js/theroom.js
+++ theroom/runtime/js/theroom.js
@@ -849,5 +849,5 @@ document.addEventListener("DOMContentLoaded", function(){
   isTouch();
   setHelpText();
   sendMessage('ready', 1);
-  if(window === window.parent) loadExcersize('butik.json');
+  if(window === window.parent) loadExcersize('teater.json');
 });
